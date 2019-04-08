b2e3320423956d62f43442d4227fe06c31668f6e
madhavan8383 <cool24bar7@gmail.com>
2019-04-08 23:12:13 +0530

    My second commit

--- src/com/sample/project/MyFirstProgram.java
+++ src/com/sample/project/MyFirstProgram.java
@@ -4,5 +4,6 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println("My first line printing");
+        System.out.println("My second line printing");
     }
 }
